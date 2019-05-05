f2406f26dcea1daf1b59be3cbf0e0b01ac7f1d30
Tyler Clark <tylerc701@hotmail.com>
2019-05-05 16:06:18 -0500

    login page add; app re-creation

---
+++ Exercise 2/source/Site/login.php
@@ -0,0 +1,25 @@
+<?php
+	//Tyler Clark
+
+    if(!isset($_SESSION)) { 
+        session_start(); 
+    }
+    include("config.php");
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $uName = $_POST['inputUName'];
+        $uPW = $_POST['inputPW'];
+        $sql = "SELECT userID, userIsAdmin FROM users WHERE userName = '$uName' and userPW = '$uPW'";
+        $result = mysql_query($sql);
+        $row = mysql_fetch_array($result);    
+        $count = mysql_num_rows($result);
+      
+        if($count == 1) {
+            echo "Login good!";
+        }else {
+            echo "Login bad!";
+        }
+	    mysql_close($db);
+        
+    } 
+?>
